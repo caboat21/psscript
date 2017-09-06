6cea5073143b0ca92371610a9efc84316624b788
caboat21 <31513637+caboat21@users.noreply.github.com>
2017-09-06 10:32:13 -0700

    Add files via upload

---
+++ Bulk group import from csv.ps1
@@ -0,0 +1,36 @@
+﻿Import-Module ActiveDirectory
+#Import CSV
+$path     = Split-Path C:\ -parent -Resolve
+$newpath  = $path + "\bulk_input.csv"
+$csv      = @()
+$csv      = Import-Csv -Path $newpath
+
+#Get Domain Base
+$searchbase = Get-ADDomain | ForEach {  $_.DistinguishedName }
+
+#Loop through all items in the CSV
+ForEach ($item In $csv)
+{
+  #Check if the OU exists
+  $check = [ADSI]::Exists("LDAP://$($item.GroupLocation),$($searchbase)")
+  
+  If ($check -eq $True)
+  {
+    Try
+    {
+      #Check if the Group already exists
+      $exists = Get-ADGroup $item.GroupName
+      Write-Host "Group $($item.GroupName) alread exists! Group creation skipped!"
+    }
+    Catch
+    {
+      #Create the group if it doesn't exist
+      $create = New-ADGroup -Name $item.GroupName -GroupScope $item.GroupType -GroupCategory Distribution -Path ($($item.GroupLocation)+","+$($searchbase))
+      Write-Host "Group $($item.GroupName) created!"
+    }
+  }
+  Else
+  {
+    Write-Host "Target OU can't be found! Group creation skipped!"
+  }
+}
---
+++ set 0365 calendar permissions.ps1
@@ -0,0 +1,4 @@
+﻿$LiveCred = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $LiveCred -Authentication Basic -AllowRedirection
+Import-PSSession $Session
+Add-MailboxFolderPermission -Identity "calender we are trying to access":\calendar -user "User WHO NEEDS ACCESS" -AccessRights Editor 
